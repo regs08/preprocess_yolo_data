dff306a5f5b722cc6f2e469729bff6ec3ffd9a35
cregs08 <cole.regnier@gmail.com>
2023-03-30 15:07:21 -0300

    adding in some print statements

--- Model/Eval/compare_models.py
+++ Model/Eval/compare_models.py
@@ -4,8 +4,9 @@ from preprocess_yolo_data.Model.Eval.calculate_mAP import get_val_test_mAP_from_
 
 def compare_mAP_models(old_model, new_model, data_yaml):
     old_model_avg_mAP = np.average(get_val_test_mAP_from_model(old_model, data_yaml))
+    print(f"#######OLD MODEL average mAP:\t {old_model_avg_mAP}\n#######")
     new_model_avg_mAP = np.average(get_val_test_mAP_from_model(new_model, data_yaml))
-
+    print(f"#######NEW MODEL average mAP:\t {new_model_avg_mAP}\n#######")
     if new_model_avg_mAP > old_model_avg_mAP:
         return True
     else:
