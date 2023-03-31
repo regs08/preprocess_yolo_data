367170bb60ece5b3757095cf5993d7616c0b4a11
cregs08 <cole.regnier@gmail.com>
2023-03-31 11:50:03 -0300

    moving some functions around for better organization

---
+++ WritingRenamingFile/from_yolo_results.py
@@ -0,0 +1,50 @@
+from preprocess_yolo_data.default_param_configs import cat_id_map
+import os
+
+
+def save_results_xyxy(results, save_dir, for_roboflow=False):
+    """
+    Save results in YOLO format
+    note that the results will not have a class_id but rather the label. this is for uploading
+    to roboflow.
+    Args:
+        results (ultralytics.yolo.engine.results.Results): Results object from YOLO
+        save_dir (str): save_folder
+    """
+    num_predictions = len(results.boxes.xywhn)
+    filename = os.path.splitext(os.path.basename(results.path))[0] + '.txt'
+    path = os.path.join(save_dir, filename)
+    with open(path, "w") as f:
+        for i in range(num_predictions):
+            #roboflow wants a label not id
+            if for_roboflow:
+                class_id = cat_id_map[int(results.boxes.cls[i])]
+            else:
+                class_id = int(results.boxes.cls[i])
+            bbox = results.boxes.xyxy[i].tolist()
+            bbox = [str(x) for x in bbox]
+            xmin,ymin,xmax,ymax = bbox
+            line = f'{class_id} {xmin} {ymin} {xmax} {ymax} \n'
+            f.write(line)
+
+
+def save_results_yolo_format(results, save_dir):
+    """
+    Save results in YOLO format
+    note that the results will not have a class_id but rather the label. this is for uploading
+    to roboflow.
+    Args:
+        results (ultralytics.yolo.engine.results.Results): Results object from YOLO
+        save_dir (str): save_folder
+    """
+    num_predictions = len(results.boxes.xywhn)
+    filename = os.path.splitext(os.path.basename(results.path))[0] + '.txt'
+    path = os.path.join(save_dir, filename)
+    with open(path, "w") as f:
+        for i in range(num_predictions):
+            class_id = int(results.boxes.cls[i])
+            bbox = results.boxes.xywhn[i].tolist()
+            bbox = [str(x) for x in bbox]
+            x, y, w, h = bbox
+            line = f'{class_id} {x} {y} {w} {h} \n'
+            f.write(line)
---
+++ WritingRenamingFile/yaml_data.py
@@ -0,0 +1,57 @@
+"""
+yaml
+"""
+import os
+import yaml
+
+
+def write_data_yaml_file(train_dir, val_dir, test_dir, class_labels, outdir):
+    """
+    :param DIRS: dict containing our  train, val, test, paths
+    :param class_labels: class labels used for training
+    :param outdir: save dir for the yaml file
+    :return:
+    """
+
+    yaml_dict = {'train': train_dir,
+                 'val': val_dir,
+                 'test': test_dir,
+                 'nc': len(class_labels),
+                 'names': class_labels}
+    yaml_path = os.path.join(outdir, 'data.yaml')
+    with open(yaml_path, 'w') as file:
+        documents = yaml.dump(yaml_dict, file)
+    return yaml_path
+
+
+def update_yaml_paths(yaml_file, DIRS):
+    """
+
+    :param yaml_file: location of the yaml file
+    :param DIRS: dictionary containing keys: tran, val, test
+    :return:
+    """
+
+    # read YAML file
+    with open(yaml_file, 'r') as file:
+        yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
+
+    # update paths
+    if 'train' in yaml_dict:
+        yaml_dict['train'] = DIRS['TRAIN']
+    else:
+        print('train line not found in yaml')
+
+    if 'val' in yaml_dict:
+        yaml_dict['val'] = DIRS['VAL']
+    else:
+        print('val line not found in yaml')
+
+    if 'test' in yaml_dict:
+        yaml_dict['test'] = DIRS['TEST']
+    else:
+        print('test line not found in yaml')
+
+    # write updated YAML file
+    with open(yaml_file, 'w') as file:
+        documents = yaml.dump(yaml_dict, file)
