155eddd9d8cb5fda563c8b7135f594faa4247d6c
cregs08 <cole.regnier@gmail.com>
2023-04-08 08:34:27 -0300

    adding in an image ext list for glob paths

--- default_param_configs.py
+++ default_param_configs.py
@@ -3,3 +3,6 @@ min_y_crop = 1280
 cat_id_map = {0: 'grape'}
 label_to_id_map = {v: k for k, v in cat_id_map.items()}
 class_labels = list(label_to_id_map.keys())
+image_exts = ['.jpg', '.png', '.jpeg']
+upper = [e.upper() for e in image_exts]
+image_exts.extend(upper)
