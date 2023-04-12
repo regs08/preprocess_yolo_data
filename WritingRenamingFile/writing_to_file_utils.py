6fe1f1ae150fc055b7cf5735af88a9fb0b00f3b2
cregs08 <cole.regnier@gmail.com>
2023-04-12 13:56:00 -0300

    test

--- WritingRenamingFile/writing_to_file_utils.py
+++ WritingRenamingFile/writing_to_file_utils.py
@@ -1,11 +1,11 @@
 import os
 
 
-def insert_string_before_extension(file_path, string_to_insert):
+def insert_string_before_extension(file_basename, string_to_insert):
     # Get the file extension
-    file_extension = os.path.splitext(file_path)[1]
+    file_extension = os.path.splitext(file_basename)[1]
     # Get the file name without extension
-    file_name = os.path.splitext(file_path)[0]
+    file_name = os.path.splitext(file_basename)[0]
     # Create new file name with string inserted before extension
     new_file_name = os.path.basename(file_name + string_to_insert + file_extension)
     # Rename the file
