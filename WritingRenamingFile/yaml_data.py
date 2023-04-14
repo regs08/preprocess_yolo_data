8b56cd7d6ca137859c2e8d8d514e2840dbad4745
cregs08 <cole.regnier@gmail.com>
2023-04-14 11:53:39 -0300

    chanign the update yaml function

--- WritingRenamingFile/yaml_data.py
+++ WritingRenamingFile/yaml_data.py
@@ -26,34 +26,32 @@ def write_data_yaml_file(dataset_folder, class_labels, outdir):
     return yaml_path
 
 
-def update_yaml_paths(yaml_file, DIRS):
+def update_yaml_paths(dataset_folder_path, yaml_file=''):
     """
 
-    :param yaml_file: location of the yaml file
-    :param DIRS: dictionary containing keys: tran, val, test
+    :param dataset_folder_path: path to the dataset containing the folders train, val, test
+    :param yaml_file: path to yaml file or defalut is os.path.join(dataset_folder_path,'data.yaml')
     :return:
     """
+    if not yaml_file:
+        yaml_file = os.path.join(dataset_folder_path, 'data.yaml')
+    folders = os.listdir(dataset_folder_path)
 
     # read YAML file
     with open(yaml_file, 'r') as file:
         yaml_dict = yaml.load(file, Loader=yaml.FullLoader)
 
+    #renaming the following keys in the yaml dict
+    data_to_rename = ['train', 'val', 'test', 'valid'] #adding in valid here for roboflow data
+
     # update paths
-    if 'train' in yaml_dict:
-        yaml_dict['train'] = DIRS['TRAIN']
-    else:
-        print('train line not found in yaml')
-
-    if 'val' in yaml_dict:
-        yaml_dict['val'] = DIRS['VAL']
-    else:
-        print('val line not found in yaml')
-
-    if 'test' in yaml_dict:
-        yaml_dict['test'] = DIRS['TEST']
-    else:
-        print('test line not found in yaml')
+    for folder in folders:
+        if folder in data_to_rename:
+            new_path = os.path.join(dataset_folder_path, folder)
+            yaml_dict[folder] = new_path
 
     # write updated YAML file
     with open(yaml_file, 'w') as file:
         documents = yaml.dump(yaml_dict, file)
+
+    return yaml_file
