4ef27a5611fc19e78889a141e40ee5eaa7d6c24e
cregs08 <cole.regnier@gmail.com>
2023-06-01 10:29:44 -0300

    adding in a check for non zeros

--- yolo2pvoc.py
+++ yolo2pvoc.py
@@ -7,5 +7,8 @@ def convert_yolo_to_pascal_voc(yolo_box, image_height, image_width, normilized=T
   ymin = int((center_y - height/2) * image_height)
   xmax = int((center_x + width/2) * image_width)
   ymax = int((center_y + height/2) * image_height)
+
   box = [xmin, ymin, xmax, ymax]
+  box = [0 if coord < 0 else coord for coord in box]
+
   return box
